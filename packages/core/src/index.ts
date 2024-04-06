7c344dd7e83555e320b0f954cffed67e0cbdb6dd
dyhalmeida <dyhalmeida@gmail.com>
2024-04-06 15:06:02 -0300

    chore: change folder name

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,5 @@
-export { Player } from "./Player";
+export { Player } from "./player";
 export { PlayerTypeEnum } from "./shared/PlayerType";
 export { Cell } from "./shared/Cell";
-export { Board } from "./Board";
-export { GameResult } from "./GameResult";
+export { Board } from "./board";
+export { GameResult } from "./game-result";
